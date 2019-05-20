e51cfc663f198f56a365941d110da6128426611d
avrahamMayerfeld <44050391+avrahamMayerfeld@users.noreply.github.com>
2019-05-19 20:21:03 -0400

    ficed github indent corruption

--- Module4phpProject/DisplayAllSingles.php
+++ Module4phpProject/DisplayAllSingles.php
@@ -1,8 +1,8 @@
 
         <?php include 'header.php';
         $conn = new mysqli('127.0.0.1','root','password121201','singles');
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+        if ($conn->connect_error) {
+                 die("Connection failed: " . $conn->connect_error);
 } 
 echo nl2br("\n ");
 echo "Connected successfully";
